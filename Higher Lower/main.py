33f308fb2423636508cf37c54bd50ec526683a99
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-04-30 22:04:32 -0500

    Add files via upload

---
+++ Higher Lower/main.py
@@ -0,0 +1,15 @@
+import random
+# TODO 1: Pull data from dictionary and present it to user
+    # Only display relevant information
+
+# TODO 2: Give user option a or b
+
+# TODO 3: Determine if user is correct
+
+# TODO 4: Make option b the new a
+
+# TODO 5: Pull new item from dictionary as option b
+
+# TODO 6: Repeat steps 2-5
+    # Make way for game to end when user is wrong
+
